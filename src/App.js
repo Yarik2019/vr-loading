8812d122e3e4624f6c20d46f3f7aeacce66c1f3d
Yarik2019 <yroslavterno@gmail.com>
2022-07-28 10:03:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+import './index.css';
+
+function App() {
+  const [isOpen, setOpen] = useState(false)
+
+	return (
+		<div>
+     hellow
+    </div>
+	)
+}
+
+export default App;
